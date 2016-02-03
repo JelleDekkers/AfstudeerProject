f4d55be44e44e60a807d724e6fcc88720fd97715
JelleDekkers <jelle.dekkers@hotmail.com>
2016-02-03 12:22:59 +0100

    Added basic destructable object scripts, objects will no break whenever they are hit with a rigidbody by enough force

---
+++ UnityMainFolder/Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetector : MonoBehaviour {
+
+	private void OnCollisionEnter(Collision col) {
+        //Debug.Log("OncollisionEnter + magn: " + col.relativeVelocity.magnitude);
+        transform.GetComponentInParent<DestructableObject>().OnCollisionDetected(col.relativeVelocity.magnitude);
+    }
+}
