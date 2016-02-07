5d1d65022fef6e5d875fa6d945f32eff6c510475
JelleDekkers <jelle.dekkers@hotmail.com>
2016-02-07 20:26:39 +0100

    Added interaction logic , you can now pick up objects and put them in your inventory.

--- UnityMainFolder/Assets/Scripts/Actors/Actor.cs
+++ UnityMainFolder/Assets/Scripts/Actors/Actor.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public enum CharacterState {
+public enum ActorState {
     ///<summary>Idle, just standing around</summary>
     Idle = 0,
     ///<summary>Alerted to an enemy presence and goes in to attack </summary> 
@@ -9,18 +9,20 @@ public enum CharacterState {
     ///<summary>patrolling after being aggroed but having lost its target</summary> 
     Patrolling = 2,
     ///<summary>Attacking an enemy </summary> 
-    InCombat = 3, 
+    InCombat = 3,
     ///<summary>Dead </summary> 
-    Dead = 4        
+    Dead = 4
 }
 
-public class Actor : MonoBehaviour {
+public class Actor : MonoBehaviour { //met interfaces IKillable, IDamageable???? met player en enemy class?
 
     public float HealthPoints { get; private set; }
     public GameObject Target { get; private set; }
+    public Inventory Inventory { get { return inventory; } }
 
-    protected int currentAlertLevel = 0; 
+    protected int currentAlertLevel = 0;
     protected float speed = 1;
     protected float fovAngle;
 
+    private Inventory inventory = new Inventory();
 }
---
+++ UnityMainFolder/Assets/Scripts/Items/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Inventory {
+
+    private List<Item> items;
+
+	public Inventory(Item[] items) {
+        this.items = items.ToList();
+    }
+
+    public Inventory() {
+    }
+
+    public void Add(Item item) {
+        Debug.Log("Adding " + item + " to inventory");
+    }
+
+    //checken of uberhaupt werkt? misschien via properties? misschien via zelf als parameter invullen welke type?
+    public List<Armor> GetAllArmor() {
+        var armors = items.OfType<Armor>();
+        return armors.ToList();
+    }
+}
