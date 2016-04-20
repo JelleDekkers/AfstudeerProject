3ebe07b2c7973bfb11a1784c4580894983eade38
JelleDekkers <jelle.dekkers@hotmail.com>
2016-04-20 16:03:12 +0200

    Improved the player camera. It now stands at a better angle and will 'peek' over walls when the view between player and camera is obstructed

---
+++ UnityMainFolder/Assets/Scripts/Cameras/PlayerCamera.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+    [SerializeField] private LayerMask wallAvoidanceLayerMask;
+
+    public float distance = 10.0f;
+    public float heightDamping = 2.0f;
+    public float minPeekDistance = 4.5f;
+    public float peekSpeed = 3f;
+    public bool isShaking;
+
+    private float wantedDistance;
+    private float wantedRotationAngle;
+    private float wantedHeight;
+    private float currentRotationAngle;
+    private float currentHeight;
+    private float shakeStrength = 4f;
+    private float shakeTimeMultiplier = 1f;
+    private float shakeTime;
+    private float shakeMinifier = 1;
+    private RaycastHit hit;
+    private Transform cameraRayCheck;
+
+    public static PlayerCamera Instance;
+
+    private void Start() {
+        if (Instance == null)
+            Instance = this;
+
+        wantedHeight = transform.position.y;
+        wantedDistance = distance;
+
+        cameraRayCheck = new GameObject().transform;
+        cameraRayCheck.parent = target.transform;
+        cameraRayCheck.position = transform.position;
+        cameraRayCheck.name = "Main Camera RayCast";
+    }
+
+    void LateUpdate() {
+        if (!target) {
+            Debug.LogWarning("No target set for PlayerCamera!");
+            return;
+        }
+
+        Debug.DrawRay(cameraRayCheck.transform.position, target.transform.position - cameraRayCheck.transform.position, Color.red);
+
+        // Calculate the current rotation angles
+        wantedRotationAngle = target.eulerAngles.y;
+        
+        currentRotationAngle = transform.eulerAngles.y;
+        currentHeight = transform.position.y;
+
+        // Damp the rotation around the y-axis
+        //currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+        currentRotationAngle = wantedRotationAngle;
+
+        // Damp the height
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        // Convert the angle into a rotation
+        Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+        // Set the position of the camera on the x-z plane
+        transform.position = target.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+
+        // Set the height of the camera
+        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+        if (Physics.Raycast(cameraRayCheck.position, Player.Instance.transform.position - cameraRayCheck.position, out hit, 100000, wallAvoidanceLayerMask)) {
+            distance = Mathf.Lerp(distance, minPeekDistance, Time.deltaTime * peekSpeed * 2);            
+        } else {
+            distance = Mathf.Lerp(distance, wantedDistance, Time.deltaTime * peekSpeed);
+        }
+
+        transform.LookAt(target);
+        //var targetRotation = Quaternion.LookRotation(targetObj.transform.position - transform.position);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+
+
+        // Shake:
+        if (isShaking) {
+            shakeTime -= Time.deltaTime;
+            transform.position = new Vector3(transform.position.x + (SmoothRandom.GetVector3Ridged(shakeStrength, shakeTimeMultiplier).x / shakeMinifier),
+                                            transform.position.y,
+                                            transform.position.z + (SmoothRandom.GetVector3Ridged(shakeStrength, shakeTimeMultiplier).z / shakeMinifier));
+            if(shakeTime < 0) {
+                isShaking = false;
+                shakeTime = 0;
+            }
+        }        
+    }
+
+    public void Shake(float time, float strength, float posMinifier) {
+        isShaking = true;
+        shakeStrength = strength;
+        shakeTime += time;
+        if (posMinifier != 0)
+            shakeMinifier = posMinifier;
+        else
+            shakeMinifier = 1;
+    }
+
+    private IEnumerator ShakeCoRoutine(float strength) {
+        isShaking = true;
+        while (shakeTime > 0) {
+            transform.position = new Vector3(transform.position.x + (SmoothRandom.GetVector3Ridged(shakeStrength, shakeTimeMultiplier).x / 2),
+                                       transform.position.y,
+                                       transform.position.z + (SmoothRandom.GetVector3Ridged(shakeStrength, shakeTimeMultiplier).z / 2));
+            print("shaking");
+            yield return new WaitForEndOfFrame();
+        }
+        isShaking = false;
+        shakeTime = 0;
+    }
+
+    /*
+    // For looking at something, maybe at character when in inventory?
+    void FixedUpdate ()
+    {
+        // if we hold Alt
+        if(Input.GetButton("Fire2") && lookAtPos)
+        {
+            // lerp the camera position to the look at position, and lerp its forward direction to match 
+            transform.position = Vector3.Lerp(transform.position, lookAtPos.position, Time.deltaTime * smooth);
+            transform.forward = Vector3.Lerp(transform.forward, lookAtPos.forward, Time.deltaTime * smooth);
+        }
+        else
+        {	
+            // return the camera to standard position and direction
+            transform.position = Vector3.Lerp(transform.position, standardPos.position, Time.deltaTime * smooth);	
+            transform.forward = Vector3.Lerp(transform.forward, standardPos.forward, Time.deltaTime * smooth);
+        }
+    }
+    */
+}
+
