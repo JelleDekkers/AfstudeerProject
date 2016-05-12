b2f92d0d67d1260d81b98665de465f463c091f8b
JelleDekkers <jelle.dekkers@hotmail.com>
2016-05-12 17:11:15 +0200

    Added test scene and script for outline shader which can be changed ingame

---
+++ UnityMainFolder/Assets/Scripts/OutlineShader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutlineShader : MonoBehaviour {
+
+    [Range(0, 0.05f)]
+    public float OutlineWidth;
+
+    private Renderer renderComponent;
+
+    private void Start() {
+        renderComponent = GetComponent<Renderer>();
+    }
+
+    private void Update() {
+        renderComponent.material.SetFloat("_Outline", OutlineWidth);
+    }
+}
